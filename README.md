7eab982f63f3e7e16db41be06e732ba50cdba697
tpauldike <topman4loveworld@gmail.com>
2023-04-13 17:31:14 +0100

    saving my works

---
+++ README.md
@@ -0,0 +1,11 @@
+# My `C` Playground
+Well, I decided to start saving some of my `c` codes (that I write while practicing `c`) on Git as well, so this directory holds such codes.
+
+While there might be some more efficient ways to solve the problems I solved here,
+I consider tracking my growth very important, so the codes that I wrote in my early
+days should not get lost, hence the purpose of this repository.
+
+I have intentions to keep my source codes clear and easily readable, so that someone else can learn from them and play with them.
+My works are arranged in different directories with names that serves as hints to contents of each directory.
+
+**Indeed *`c` is fun***
---
+++ register_buddy_group/reg.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[10][25], pass, grp[20];
+    char fname[25];
+    int s = 0, cdwn = 9;
+
+    printf("Enter 'r' to register your buddy group or 'q' to exit: \n");
+    scanf("%c", &pass);
+
+    if (pass != 'r')
+    {
+	puts("Do have a nice day");
+	return -1;
+    }
+    puts("What is the name of your buddy group?\n(no space(s); just a word)");
+    scanf("%s", grp);
+    puts("\nYou may now register your members\n(maximum of 10)");
+
+    while (s < 10)
+    {
+	puts("Enter the firstname or 'q' to stop:");
+	scanf("%s", fname);
+	strcpy(str[s], fname);
+
+	if (cdwn == 1)
+	    printf("\n%d chance left.\n", cdwn);
+	else if (cdwn == 0)
+	{
+	    printf("\nYou have reached the max\n");
+	    break;
+	}
+	else if (!strcmp(fname, "q"))
+	{
+	    strcpy(str[s], "\0");
+	    break;
+	}
+	else
+	    printf("\n%d chances left.\n", cdwn);
+	s += 1;
+	cdwn -= 1;
+    }
+    printf("\nDone with Registration!\n---\t---\t---\nRegistered Members of %s\n", grp);
+    for (s = 0; s < 10; s++)
+    {
+	if (!strcmp(str[s], "\0"))
+	    break;
+	printf("%d. ", s+1);
+	printf("%s\n", str[s]);
+    }
+    return (0);
+}
